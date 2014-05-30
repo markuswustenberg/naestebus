df80b2e6e9c3aadd6e0b8d72a67101ed0311b0b8
Markus Wüstenberg <markus@wustenberg.dk>
2014-05-30 18:13:02 +0200

    More cleanup.

--- app/src/main/webapp/js/script.js
+++ app/src/main/webapp/js/script.js
@@ -1,52 +1,25 @@
-var app = {
-    ROOT: location.protocol + '//' + location.hostname,
-    MAP_CENTER: new google.maps.LatLng(56.172113, 10.188317),
+var ROOT = location.protocol + '//' + location.hostname;
 
-    map: null,
-    infoWindow: new google.maps.InfoWindow(),
-    me: null,
+var app = {
     stops: {}
 }
 
-function Stop(data) {
-    this.data = data;
-    this.marker = new google.maps.Marker({
-        position: new google.maps.LatLng(data.latitude / 1000000, data.longitude / 1000000),
-        title: data.name,
-        map: app.map,
-        icon: app.ROOT + "/img/littlebus.png"
-    });
-    var self = this;
-    google.maps.event.addListener(this.marker, 'click', function() {
-        self.updateAndShowInfoWindow();
-    });
-}
-Stop.prototype.updateAndShowInfoWindow = function() {
-    var self = this;
-    $.getJSON(app.ROOT + "/departures?stopId=" + this.data.id + '&max=5', function(departures) {
-        var content = '';
-        departures.forEach(function(departure) {
-            content += '<p><strong>' + departure.name + '</strong> ' + departure.time;
-            if (departure.hasDirection) {
-                content += ' → ' + departure.direction;
-            }
-            content += '</p>';
-        });
-        updateInfoWindow(self.marker, content);
-    });
-}
-
-function Me(position) {
+function Me(map) {
     // Add marker
+    this.map = map;
     this.marker = new google.maps.Marker({
-        position: position,
-        map: app.map,
-        icon: app.ROOT + "/img/littleperson.png",
+        position: map.getCenter(),
+        map: map,
+        icon: ROOT + "/img/littleperson.png",
         draggable: true
     });
 
     // Add info window telling the user this marker can be moved
-    updateInfoWindow(this.marker, 'You can move me!');
+    var infoWindow = new google.maps.InfoWindow({content: 'You can move me!'});
+    infoWindow.open(map, this.marker);
+    setTimeout(function() {
+        infoWindow.close();
+    }, 3000);
 
     // Add position listener
     var self = this;
@@ -56,74 +29,86 @@ function Me(position) {
 
     this.onPositionUpdate();
 }
-Me.prototype.setPosition = function(position) {
-    this.marker.position = position;
-    this.onPositionUpdate();
-}
 Me.prototype.onPositionUpdate = function() {
+    this.map.panTo(this.marker.getPosition());
+    this.findStops();
+}
+Me.prototype.findStops = function() {
+    // Find and add stop markers
     var position = this.marker.getPosition();
-    app.map.panTo(position);
-    fetchStops(position);
-}
-
-function initialize() {
-    var mapOptions = {
-        mapTypeControlOptions: {
-            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'grayscale']
-        },
-        center: app.MAP_CENTER,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 15
-    };
-    app.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    var styles = [
-        {
-            "stylers": [
-                { "saturation": -100 }
-            ]
-        }
-    ];
-    var mapType = new google.maps.StyledMapType(styles, { name:"Gray" });
-    app.map.mapTypes.set('grayscale', mapType);
-    app.map.setMapTypeId('grayscale');
-
-    // Close info window if clicked outside
-    google.maps.event.addListener(app.map, "click", function(event) {
-        app.infoWindow.close();
-    });
-
-    // Add me
-    app.me = new Me(app.map.getCenter());
-
-    // Find current location if possible and center
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            app.me.setPosition(pos);
-        });
-    }
-}
-
-function updateInfoWindow(marker, content) {
-    // Close the previous one, set content, and open again with new content
-    app.infoWindow.close();
-    app.infoWindow.setContent(content);
-    app.infoWindow.open(app.map, marker);
-}
-
-function fetchStops(position) {
-    // Fetch and add stop markers
-    $.getJSON(app.ROOT + "/stops?latitude=" + ((position.lat() * 1000000) | 0) + "&longitude=" + ((position.lng() * 1000000) | 0) + "&radius=1000&max=5", function(data) {
-        data.forEach(function(datum) {
+    var map = this.map;
+    $.getJSON(ROOT + "/stops?latitude=" + ((position.lat() * 1000000) | 0) + "&longitude=" + ((position.lng() * 1000000) | 0) + "&radius=1000&max=5", function(stops) {
+        stops.forEach(function(stop) {
             // Check if the marker is already present
-            var key = datum.latitude.toString() + datum.longitude.toString();
+            var key = stop.latitude.toString() + stop.longitude.toString();
             if (key in app.stops) {
                 return;
             }
-
-            app.stops[key] = new Stop(datum);
+            app.stops[key] = new Stop(map, stop);
         });
     });
+}
+
+function Stop(map, data) {
+    this.map = map;
+    this.data = data;
+    this.marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.latitude / 1000000, data.longitude / 1000000),
+        title: data.name,
+        map: map,
+        icon: ROOT + "/img/littlebus.png"
+    });
+    var self = this;
+    google.maps.event.addListener(this.marker, 'click', function() {
+        self.updateAndShowInfoWindow();
+    });
+}
+Stop.infoWindow = new google.maps.InfoWindow();
+Stop.prototype.updateAndShowInfoWindow = function() {
+    var self = this;
+    $.getJSON(ROOT + "/departures?stopId=" + this.data.id + '&max=5', function(departures) {
+        var content = '';
+        departures.forEach(function(departure) {
+            content += '<p><strong>' + departure.name + '</strong> ' + departure.time;
+            if (departure.hasDirection) {
+                content += ' → ' + departure.direction;
+            }
+            content += '</p>';
+        });
+
+        // Close the previous one, set content, and open again with new content
+        Stop.infoWindow.close();
+        Stop.infoWindow.setContent(content);
+        Stop.infoWindow.open(self.map, self.marker);
+    });
+}
+
+function initialize() {
+    var map = new google.maps.Map(document.getElementById("map-canvas"), {
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'grayscale']
+        },
+        center: new google.maps.LatLng(56.172113, 10.188317),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoom: 15
+    });
+    map.mapTypes.set('grayscale', new google.maps.StyledMapType([{stylers: [{saturation: -100}]}], {name: "Gray"}));
+    map.setMapTypeId('grayscale');
+
+    // Close stop info windows if clicked outside
+    google.maps.event.addListener(map, "click", function(event) {
+        Stop.infoWindow.close();
+    });
+
+    // Find current location if possible
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+            new Me(map);
+        });
+    } else {
+        new Me(map);
+    }
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);

8f09f2440e451cf140499a1becdfd024b393d549
Markus Wüstenberg <markus@wustenberg.dk>
2014-06-04 20:40:14 +0200

    Removed detailed departure tests, the backend changes too frequently.

--- app/src/test/java/com/nexthighspeedmetaltube/datasupplier/TestRejseplanDataSupplier.java
+++ app/src/test/java/com/nexthighspeedmetaltube/datasupplier/TestRejseplanDataSupplier.java
@@ -4,14 +4,13 @@ import com.google.common.collect.ImmutableList;
 import com.nexthighspeedmetaltube.model.Coordinate;
 import com.nexthighspeedmetaltube.model.Departure;
 import com.nexthighspeedmetaltube.model.Stop;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeUtils;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -24,21 +23,12 @@ public class TestRejseplanDataSupplier {
     private static final int NEARBY_STOPS_STOP_COUNT = 2;
 
     private static final int DEPARTURES_STOP_COUNT = 1;
-    private static final DateTime DEPARTURES_TIME = new DateTime("2014-05-30T13:36:00+02:00");
 
     private static final String STOP1_NAME = "Paludan-Müllers Vej/Ekkodalen (Aarhus)";
     private static final Coordinate STOP1_COORDINATE = new Coordinate(56172728, 10183438);
 
     private static final String STOP2_NAME = "Paludan-Müllers Vej/Åbogade (Aarhus)";
     private static final Coordinate STOP2_COORDINATE = new Coordinate(56170247, 10186692);
-
-    private static final String DEPARTURE1_NAME = "Bus 2A";
-    private static final DateTime DEPARTURE1_TIME = new DateTime("2014-05-30T13:37:00+02:00");
-    private static final String DEPARTURE1_DIRECTION = "Bjødstrupvej/Karetmagertoften (Aarhus)";
-
-    private static final String DEPARTURE2_NAME = DEPARTURE1_NAME;
-    private static final DateTime DEPARTURE2_TIME = new DateTime("2014-05-30T13:43:00+02:00");
-    private static final String DEPARTURE2_DIRECTION = "Aarhus Uni. hospital  Skejby indg 8-11";
 
     private DataSupplier dataSupplier;
 
@@ -74,18 +64,9 @@ public class TestRejseplanDataSupplier {
         // We need an id that changes frequently according to spec
         ImmutableList<Stop> stops = dataSupplier.getNearbyStops(UBER_OFFICE_COORDINATE, NEARBY_STOPS_RADIUS, DEPARTURES_STOP_COUNT);
 
-        DateTimeUtils.setCurrentMillisFixed(DEPARTURES_TIME.getMillis());
         ImmutableList<Departure> departures = dataSupplier.getNextDepartures(stops.get(0).getId());
 
-        // Test a few
-        Departure departure1 = departures.get(0);
-        assertEquals(DEPARTURE1_NAME, departure1.getName());
-        assertEquals(DEPARTURE1_TIME, departure1.getTime());
-        assertEquals(DEPARTURE1_DIRECTION, departure1.getDirection());
-
-        Departure departure2 = departures.get(1);
-        assertEquals(DEPARTURE2_NAME, departure2.getName());
-        assertEquals(DEPARTURE2_TIME, departure2.getTime());
-        assertEquals(DEPARTURE2_DIRECTION, departure2.getDirection());
+        // We can only really test that there are some departures, as they change frequently
+        assertFalse(departures.isEmpty());
     }
 }

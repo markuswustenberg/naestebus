5570e8648c3e5021b3d8171bfeee6f76321a5d9b
Markus Wüstenberg <markus@wustenberg.dk>
2014-06-24 15:16:19 +0200

    Logging in server.

--- app/src/main/java/dk/naestebus/Main.java
+++ app/src/main/java/dk/naestebus/Main.java
@@ -2,6 +2,8 @@ package dk.naestebus;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FilenameFilter;
@@ -14,17 +16,29 @@ public class Main {
     public static final String WAR_PATH = "./build/libs/";
     public static final String WAR_FILE_ENDING = "war";
 
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String... args) throws Exception {
-        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-        WebAppContext context = new WebAppContext();
-        context.setContextPath("/");
         // Find war file
-        File[] wars = new File(WAR_PATH).listFiles(new FilenameFilter() {
+        File warPath = new File(WAR_PATH);
+
+        log.info("Starting server with war path {}...", warPath.getAbsoluteFile());
+
+        File[] wars = warPath.listFiles(new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
                 return name.endsWith(WAR_FILE_ENDING);
             }
         });
+
+        if (wars.length == 0) {
+            log.error("No war file found, exiting.");
+            return;
+        }
+
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        WebAppContext context = new WebAppContext();
+        context.setContextPath("/");
         context.setWar(wars[0].getAbsolutePath());
         server.setHandler(context);
         server.start();
